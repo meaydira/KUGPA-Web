5289f6aab29ba7e21f4d59649c3c30221193bc7c
DILARA BOZYILAN <dbozyilan16@ku.edu.tr>
2020-02-05 22:53:40 +0300

    heroku bug fix

--- src/main/java/com/dilaraapps/kugpa/KUGPAGetGradesController.java
+++ src/main/java/com/dilaraapps/kugpa/KUGPAGetGradesController.java
@@ -25,24 +25,9 @@ public class KUGPAGetGradesController {
     public ResponseEntity getGrades(@RequestParam(value="username") String username, @RequestParam(value="passw") String password) {
 
         Student current = null;
-        try {
-
-            current = gpaCalculator.getStudent(username, password);
-            // System.out.println(current.getSPAForAll().keySet().toString());
-
-
-     /*       model.addAttribute("courses", current.getCoursesTaken());
-            model.addAttribute("nameSurname", current.getNameSurname());
-            model.addAttribute("courseList", current.getCoursesPerTerm());// Hashmap<String,List<Course>>
-            model.addAttribute("SPAList", current.getSPAForAll()); //HashMap <String,Float>
-            model.addAttribute("termIncluded", current.getNonNullSPATerms()); //ArrayList -String
-            model.addAttribute("terms", current.getTerms()); //ArrayList -String
-            model.addAttribute("gpa", current.getCumulativeGPA());
-            model.addAttribute("totalCredit", current.getTotalCredits());  */
-
+        try {current = gpaCalculator.getStudent(username, password);
             return ResponseEntity.ok(current);
         } catch (Exception e) {
-           // model.addAttribute("errorMessage", e.getMessage());
             System.out.println(e);
             return ResponseEntity.ok(current);
         }
@@ -56,14 +41,15 @@ public class KUGPAGetGradesController {
         try {
             String username = user.getUsername();
             String password = user.getPassw();
-         /*   if(requestBody.has("username")){
-                username = requestBody.get("username").toString();
-            }
-            if(requestBody.has("passw")){
-                username = requestBody.get("passw").toString();
-            }  */
             current = gpaCalculator.getStudent(username, password);
-            // System.out.println(current.getSPAForAll().keySet().toString());
+            return ResponseEntity.ok(current);
+        } catch (Exception e) {
+            System.out.println(e);
+            return ResponseEntity.ok(current);
+        }
+
+    }
+
 
 
      /*       model.addAttribute("courses", current.getCoursesTaken());
@@ -74,14 +60,5 @@ public class KUGPAGetGradesController {
             model.addAttribute("terms", current.getTerms()); //ArrayList -String
             model.addAttribute("gpa", current.getCumulativeGPA());
             model.addAttribute("totalCredit", current.getTotalCredits());  */
-
-            return ResponseEntity.ok(current);
-        } catch (Exception e) {
-            // model.addAttribute("errorMessage", e.getMessage());
-            System.out.println(e);
-            return ResponseEntity.ok(current);
-        }
-
-    }
 
 }
